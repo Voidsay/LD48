27612b6a5beb70a1008982b7dd8a86787c84ca7a
Voidsay <63986704+Voidsay@users.noreply.github.com>
2021-04-25 22:41:48 +0200

    Add files via upload

---
+++ Assets/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(End());
+    }
+
+
+    IEnumerator End()
+    {
+        yield return new WaitForSeconds(5);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        PlayerPrefs.SetInt("Checkpoint", 0);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
